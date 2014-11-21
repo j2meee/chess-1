b22d41b06066206b6f932523036b3820f3ac5b56
sneivandt <stuartlneivandt@gmail.com>
2014-11-20 19:02:19 -0600

    win32 compatability

--- main.cc
+++ main.cc
@@ -46,7 +46,7 @@ int main()
         }
         else if(input == "s") {
             std::cout << std::endl;
-            SearchInfo info(7);
+            SearchInfo info(-1);
             search::searchPosition(pos, info);
         }
         else if(input == "p") {
--- utils.cc
+++ utils.cc
@@ -12,5 +12,22 @@ std::string utils::getTimestamp()
 
 long utils::getTime()
 {
+    #ifdef WIN32
+    SYSTEMTIME time;
+    GetSystemTime(&time);
+    return time.wMilliseconds;
+    #else
     return std::chrono::system_clock::now().time_since_epoch()/std::chrono::milliseconds(1);
+    #endif
+}
+
+bool utils::inputWaiting()
+{
+    fd_set fds;
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 1;
+    FD_ZERO(&fds);
+    FD_SET(fileno(stdin), &fds);
+    return select(sizeof(fds)*8, &fds, NULL, NULL, &tv);
 }
